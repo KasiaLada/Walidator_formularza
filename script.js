6e7cff39aa1f8d274f8d2164d640081ecf3916ec
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-02-20 22:07:30 +0100

    added sendBtn

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const username = document.querySelector("#username");
-const pass = document.querySelector("#password");
-const pass2 = document.querySelector("#password2");
-const email = document.querySelector("#email");
-const sendBtn = document.querySelector(".send");
-const clearBtn = document.querySelector(".clear");
-const popup = document.querySelector(".popup");
+const username = document.querySelector('#username');
+const pass = document.querySelector('#password');
+const pass2 = document.querySelector('#password2');
+const email = document.querySelector('#email');
+const sendBtn = document.querySelector('.send');
+const clearBtn = document.querySelector('.clear');
+const popup = document.querySelector('.popup');
 
-
-const  checkform = input => {
-console.log(input);
+const checkform = (input) => {
+	console.log(input);
+};
+sendBtn.addEventListener(click, e=>{
+	e.preventDefault();
 }
+	)
+clearBtn.addEventListener('click', (e) => {
+	e.preventDefault();
 
-
-clearBtn.addEventListener("click", e=>{
-    e.preventDefault();
-    [username, pass, pass2, email].forEach(el =>{
-        el.value = "";
-    })
-})
+	[username, pass, pass2, email].forEach((el) => {
+		el.value = '';
+	});
+});
